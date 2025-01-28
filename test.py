aad560f9035e651b6bb4ed021c5307518e70f4f7
Seraphania <strings333@hotmail.com>
2025-01-28 10:53:33 +0800

    Stufff and things

--- test.py
+++ test.py
@@ -1,46 +1,21 @@
-import matplotlib.pyplot as plt
-# matplotlib inline
+import sys
 
-# Create a class Circle
-class Circle(object):
-    
-    # Constructor
-    def __init__(self, radius=3, color='blue'):
-        self.radius = radius
-        self.color = color 
-    
-    # Method
-    def add_radius(self, r):
-        self.radius = self.radius + r
-        return(self.radius)
-    
-    # Method
-    def drawCircle(self):
-        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
-        plt.axis('scaled')
-        plt.show()  
+def safe_divide(n,d):
+    try:
+        print(f"{n}/{d} = {n/d}")
+    except ValueError:
+        print("Please enter a valid number: ")
+        sys.exit(1)
+    except ZeroDivisionError:
+        print("Error: Cannot divide by Zero!")
+        d = int(input("Please enter another number: "))
+        safe_divide(n,d)
+    except:
+        print("Something went wrong")
+        sys.exit(1)
 
+n = int(input("Please enter a numerator: "))
+d = int(input("Please enter a denominator: "))
+safe_divide(n,d)
 
-RedCircle = Circle(10, 'red')
-# Print the object attribute radius
-print(RedCircle.radius)
-
-# Print the object attribute color
-print(RedCircle.color)
-
-# Set the object attribute radius
-RedCircle.radius = 1
-print(RedCircle.radius)
-
-# Display methods for RedCircle
-print(dir(RedCircle))
-
-# Call the method drawCircle
-RedCircle.drawCircle()
-
-# Use method to change the object attribute radius
-print('Radius of object:',RedCircle.radius)
-RedCircle.add_radius(2)
-print('Radius of object of after applying the method add_radius(2):',RedCircle.radius)
-RedCircle.add_radius(5)
-print('Radius of object of after applying the method add_radius(5):',RedCircle.radius)
+### Does not actually handle ValueError because n/d are cast to ints outide the function. To actually use the `sys.exit(1)` cast to ints before trying the division; or the better option, make a `get_valid_integer()` function.
